6476cd0973834ea2807529c23ee2f525ef6d9073
rsecchi <r.secchi@gmail.com>
2023-02-08 10:27:36 -0500

    Adding Hystart++ to Cubic

--- satlink.cc
+++ satlink.cc
@@ -8,15 +8,19 @@
 #include "ns3/bulk-send-application.h"
 #include "ns3/config-store.h"
 #include "ns3/tcp-socket-base.h"
+#include "ns3/traffic-control-layer.h"
+#include "ns3/fq-codel-queue-disc.h"
+#include "ns3/packet.h"
 
 #include <fstream>
+#include <memory>
 #include <string>
 
 
 using namespace ns3;
 
 
-void list_aggreagates(Ptr<Object> p) {
+void list_aggregates(Ptr<Object> p) {
 	Object::AggregateIterator it = p->GetAggregateIterator();
 	while(it.HasNext())
 		std::cout << it.Next()->GetInstanceTypeId().GetName() << std::endl;
@@ -25,17 +29,32 @@ void list_aggreagates(Ptr<Object> p) {
 
 uint32_t num_flows = 1;
 std::ofstream cwnd[10];
+std::ofstream rtt[10];
+std::ofstream bttln_queue;
 
-
-/* function callback to printe the congestion window */
+/* function callback to print the congestion window */
 static void CwndTracer(std::string context, uint32_t oldval, uint32_t newval)
 {
-
 	uint32_t socket = int(context[43] - 48);
 	cwnd[socket] << Simulator::Now().GetSeconds() << " " << oldval << std::endl;
 	cwnd[socket] << Simulator::Now().GetSeconds() << " " << newval << std::endl;
 
 }
+
+static void RWNDTracer(std::string context, uint32_t oldval, uint32_t newval)
+{
+ 	std::cout << Simulator::Now().GetSeconds() << " " << oldval << std::endl;
+
+}
+
+/* function callback to print the RTT */
+static void RttTracer(std::string context, Time oldval, Time newval)
+{
+	uint32_t socket = int(context[43] - 48);
+	rtt[socket] << Simulator::Now().GetSeconds() << " " << oldval.GetSeconds() << std::endl;
+	rtt[socket] << Simulator::Now().GetSeconds() << " " << newval.GetSeconds() << std::endl;
+}
+
 
 /* function callback when a transfer is completed*/
 void Completed(Ptr<Socket> sock)
@@ -57,6 +76,8 @@ void SocketTraces(uint32_t i, Ptr<BulkSendApplication> bulk) {
 		"/NodeList/0/$ns3::TcpL4Protocol/SocketList/" +	std::to_string(i);
 
 	Config::Connect(name + "/CongestionWindow", MakeCallback(&CwndTracer));
+	Config::Connect(name + "/RTT", MakeCallback(&RttTracer));
+	// Config::Connect(name + "/RWND", MakeCallback(&RWNDTracer));
 
 	/* access to the socket */
 	Ptr<Socket> socket = bulk->GetSocket();
@@ -65,6 +86,12 @@ void SocketTraces(uint32_t i, Ptr<BulkSendApplication> bulk) {
 				MakeNullCallback<void, Ptr<Socket>>());
 }
 
+void traceenqueue(Ptr<const QueueDisc> qd)
+{
+	bttln_queue << Simulator::Now().GetSeconds() <<  " " ;
+	bttln_queue << qd->GetNPackets() << std::endl;
+	Simulator::Schedule(MilliSeconds(1), &traceenqueue, qd);
+}
 
 int main(int argc, char* argv[])
 {
@@ -74,12 +101,19 @@ int main(int argc, char* argv[])
 	double error_p = 0.0;
 	double duration = 100.0;
 	double offset = 0.001;
-	uint64_t data_mbytes = 0;
+	uint64_t data_kbytes = 1000;
+	uint32_t iw = 10;
+	bool boolp = false;
+	bool boolp_iw = false;
+
 	std::string transport_prot = "TcpCubic";
 	std::string bandwidth = "5Mbps";
 	std::string delay = "45ms";
 	std::string qlen = "100p";
 	std::string mtu = "1500";
+	std::string pacing = "0";
+	std::string pacing_iw = "0";
+	std::string ss_pacing_ratio = "200";
 
 	CommandLine cmd(__FILE__);
 	cmd.AddValue("num_flows", "Number of flows", num_flows);
@@ -90,18 +124,38 @@ int main(int argc, char* argv[])
 	cmd.AddValue("mtu", "Packet size", mtu);
 	cmd.AddValue("error_p", "Packet error rate", error_p);
 	cmd.AddValue("duration", "duration", duration);
-	cmd.AddValue("data", "Number of MBs to send", data_mbytes);
+	cmd.AddValue("data", "Number of kBs to send", data_kbytes);
 	cmd.AddValue("seed", "Random numbers seed", seed);
 	cmd.AddValue("offset", "Spacing between connections", offset);
+	cmd.AddValue("iw", "Initial congestion window", iw);
+	cmd.AddValue("pacing", "Enable Pacing (0/1)", pacing);
+	cmd.AddValue("pacing_iw", "Enable Pacing for IW (0/1)", pacing_iw);
+	cmd.AddValue("ss_pacing_ratio", "pacing perc. increase in SS", ss_pacing_ratio);
 	cmd.Parse(argc, argv);
 
+	if (pacing == "1")
+		boolp = true;
+
+	if (pacing_iw == "1")
+		boolp_iw = true;
+
 	/* config */
-	printf("num_flows=%d\n", num_flows);
+	/* pacing */
+	Config::SetDefault("ns3::TcpSocketState::EnablePacing", BooleanValue(boolp));
+	Config::SetDefault("ns3::TcpSocketState::PaceInitialWindow", BooleanValue(boolp_iw));
+	Config::SetDefault("ns3::TcpSocketState::PacingSsRatio", UintegerValue(stoi(ss_pacing_ratio)));
+	
+	Config::SetDefault("ns3::TcpSocket::InitialCwnd", UintegerValue(iw));
+	Config::SetDefault("ns3::TcpSocket::RcvBufSize", UintegerValue(327680000));
+	Config::SetDefault("ns3::TcpSocket::SndBufSize", UintegerValue(327688000));
+	Config::SetDefault("ns3::TcpSocketBase::MaxWindowSize", UintegerValue(65534));
+	Config::SetDefault("ns3::TcpSocketBase::WindowScaling", BooleanValue(true));
+	Config::SetDefault("ns3::FqCoDelQueueDisc::MaxSize", StringValue(qlen) );
+
 	transport_prot = "ns3::" + transport_prot;
 	Config::SetDefault("ns3::TcpL4Protocol::SocketType",
 		TypeIdValue(TypeId::LookupByName(transport_prot)));
 	Config::SetDefault("ns3::TcpSocket::SegmentSize", UintegerValue(std::stoi(mtu)));
-
 
 	SeedManager::SetSeed(seed);
 	SeedManager::SetRun(run);
@@ -126,23 +180,36 @@ int main(int argc, char* argv[])
 	pp.SetDeviceAttribute("ReceiveErrorModel", PointerValue(&error_model));
 	NetDeviceContainer devs = pp.Install(nodes);
 
-	/* assign addresses */
+	/* Create Internet stack (TCP/IP and assign addresses */
 	InternetStackHelper stack;
 	stack.Install(nodes);
 	Ipv4AddressHelper address;
 	address.SetBase("10.1.1.0", "255.255.255.0");
 	Ipv4InterfaceContainer ifv4 = address.Assign(devs);
-	pp.EnablePcapAll("tcpsat");
+	// pp.EnablePcapAll("tcpsat");
+
+
+	//list_aggregates(nodes.Get(0));
+	bttln_queue.open("bottln_queue.tr", std::ios::out);
+	Ptr<TrafficControlLayer> tc = nodes.Get(0)->GetObject<TrafficControlLayer>();
+	Ptr<QueueDisc> qdisc = tc->GetRootQueueDiscOnDevice(devs.Get(0));
+	Simulator::Schedule(Seconds(0), &traceenqueue, qdisc);
+	
+	// Connect to a QueueDisc "Drop" tracer
+	//Config::ConnectWithoutContext(
+	//	"/NodeList/0/$ns3::TrafficControlLayer/RootQueueDiscList/0/Drop",
+	//	Make(&traceenqueue));
 
 	/* create sources */	
 	ApplicationContainer apps;
 	for(uint32_t i=0; i<num_flows; i++) {
 		InetSocketAddress daddr = InetSocketAddress(ifv4.GetAddress(1), i+9);
 		BulkSendHelper src("ns3::TcpSocketFactory", daddr);
-		src.SetAttribute("MaxBytes", UintegerValue(data_mbytes*1000000));
+		src.SetAttribute("MaxBytes", UintegerValue(data_kbytes*1000));
 		apps = src.Install(nodes.Get(0));
 		apps.Start(Seconds(i*offset));
 		cwnd[i].open("cwnd" + std::to_string(i) + ".tr", std::ios::out);
+		rtt[i].open("rtt" + std::to_string(i) + ".tr", std::ios::out);
 	
 		/* get reference to bulk application */
 		Ptr<BulkSendApplication> bulk;
@@ -160,12 +227,12 @@ int main(int argc, char* argv[])
 	}
 
 	/* start simulation */
-	FlowMonitorHelper fmon;
-	fmon.InstallAll();
+	// FlowMonitorHelper fmon;
+	// fmon.InstallAll();
 	Simulator::Stop(Seconds(duration));
 	Simulator::Run();
 
-	fmon.SerializeToXmlFile("tcpsat.flowmonitor", true, true);
+	// fmon.SerializeToXmlFile("tcpsat.flowmonitor", true, true);
 	Simulator::Destroy();
 
 	return 0;
---
+++ tcp_preliminary.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+#  Script Usage
+#  ./ns3 run scratch/satlink.cc -- --PrintHelp 
+#satlink [Program Options] [General Arguments]
+#
+#Program Options:
+#    --num_flows:       Number of flows [1]
+#    --bandwidth:       Bottleneck bandwidth [5Mbps]
+#    --delay:           Bottleneck delay (ms) [45ms]
+#    --transport_prot:  Transport protocol [TcpCubic]
+#    --qlen:            Queue Length [100p]
+#    --mtu:             Packet size [1500]
+#    --error_p:         Packet error rate [0]
+#    --duration:        duration [100]
+#    --data:            Number of kBs to send [0]
+#    --seed:            Random numbers seed [1]
+#    --offset:          Spacing between connections [0.001]
+#
+#General Arguments:
+#    --PrintGlobals:              Print the list of globals.
+#    --PrintGroups:               Print the list of groups.
+#    --PrintGroup=[group]:        Print all TypeIds of group.
+#    --PrintTypeIds:              Print all TypeIds.
+#    --PrintAttributes=[typeid]:  Print all attributes of typeid.
+#    --PrintVersion:              Print the ns-3 version.
+#    --PrintHelp:                 Print this help message.
+#
+
+
+SCRIPT="scratch/satlink"
+ARG=""
+
+# 5 Mbps, 50ms --> BDP = 42 pkt
+ARG="$ARG --num_flows=1"
+ARG="$ARG --delay=50ms"
+ARG="$ARG --qlen=55p"
+
+
+echo "Preliminary simulations with TCP CCs"
+NS3_DIR="/home/raffaello/workspace/ns-allinone-3.37/ns-3.37/"
+NS3="/home/raffaello/workspace/ns-allinone-3.37/ns-3.37/ns3"
+
+RANGE=$(seq 50 50 1000)
+
+# Bad scenario
+#ARG="$ARG --num_flows=2"
+ARG="$ARG --error_p=0.02"
+RANGE=$(seq 5 5 1000)
+
+
+
+echo Standard Slow Start
+TP=TcpNewReno
+for i in $RANGE;
+do
+	CT=$($NS3 run "$SCRIPT $ARG --transport_prot=$TP --data=$i --seed=$i")
+	echo $i $CT
+done > static_$TP.tr
+cp $NS3_DIR/cwnd0.tr static_cwnd_$TP.tr
+cp $NS3_DIR/rtt0.tr static_rtt_$TP.tr
+
+echo Classical Hystart
+TP=TcpCubic
+for i in $RANGE;
+do
+	CT=$($NS3 run "$SCRIPT $ARG --transport_prot=$TP --data=$i --seed=$i")
+	echo $i $CT
+done > static_$TP.tr
+cp $NS3_DIR/cwnd0.tr static_cwnd_$TP.tr
+cp $NS3_DIR/rtt0.tr static_rtt_$TP.tr
+
+echo IW=42
+TP=TcpNewReno
+ARG="$ARG --iw=42"
+for i in $RANGE;
+do
+	CT=$($NS3 run "$SCRIPT $ARG --transport_prot=$TP --data=$i --seed=$i")
+	echo $i $CT
+done > static_${TP}_iw42.tr
+cp $NS3_DIR/cwnd0.tr static_cwnd_${TP}_iw42.tr
+cp $NS3_DIR/rtt0.tr static_rtt_${TP}_iw42.tr
+
+
