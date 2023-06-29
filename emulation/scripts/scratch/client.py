87a9106ffc5b964ae7d3c7c127c8b9e1eb20f317
janev94 <misho.janew@gmail.com>
2023-06-29 13:27:49 +0100

    Client terminates when it receives a number of bytes given as an input argument

--- emulation/scripts/scratch/client.py
+++ emulation/scripts/scratch/client.py
@@ -6,5 +6,8 @@ client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 client.connect((sys.argv[1], 80))
 
-while True:
-    len_recvd = client.recv(2048)
+expecting = int(sys.argv[2])
+
+while expecting > 0:
+    len_recvd = len(client.recv(2048))
+    expecting -= len_recvd
